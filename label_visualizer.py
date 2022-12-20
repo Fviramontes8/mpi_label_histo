5074ba37ccffe7b390d9ff5f4065ceaacfb288fd
Francisco Viramontes <fviramontes8@unm.edu>
2022-12-20 12:54:28 -0700

    Add label_visualizer
    Added python script to read the results generated by the cmake project.
    It reads in the contents of the file and creates a bar graph
    representing the distribution of the classes

---
+++ label_visualizer.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main() -> None:
+    with open("sequential_histo.txt") as infile:
+        file_data = infile.readlines()
+        list_data = file_data[0].split(' ')
+
+    list_data = list_data[:30]
+    np_data = np.array(list_data, dtype=int)
+    classes = [ i for i in range(30) ]
+
+    plt.bar(classes, np_data)
+    plt.title("Histogram of classes")
+    plt.xlabel("Objects of interest (Classes)")
+    plt.ylabel("Number of occurrences in dataset")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
