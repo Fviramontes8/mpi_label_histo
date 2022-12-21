37342a3ca48b6b5c65aafc2fbeb6dafaf48e60aa
Francisco Viramontes <fviramontes8@unm.edu>
2022-12-21 14:38:47 -0700

    Delete histo_from_dir
    Removed files relevant to the sequential calculation of the histogram as
    it will all be defered to the mpi version

--- include/histo_from_dir.hpp
+++
@@ -1,6 +0,0 @@
-#include <array>
-#include <filesystem>
-#include <regex>
-
-void histo_from_dir(const std::filesystem::directory_iterator& dir,
-	const std::regex& expr, std::array<int, 30>& histo);
