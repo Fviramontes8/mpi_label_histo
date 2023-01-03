86fdcfd7248fab0fce5b6c57bc735cc55878e634
Francisco Viramontes <fviramontes8@unm.edu>
2023-01-03 16:09:49 -0700

    Update main application
    The application now has support for command line arguments. In fact, it
    is required. Users have three options:
        Display a help message with --help or -h
        Specify a filepath with --path or -p. This is a required argument.
        Specify a size for the histogram with --size or -s.
    	This is a required argument.
    
    If the filepath or size argument are not specified, the program will
    throw an exception. An exception is thrown if the path provided does not
    exist. A helper print statement is provided.
    
    The regex statement has been changed to match the pattern of a number
    rather than a specific range of numbers.

--- app/app.cpp
+++ app/app.cpp
@@ -1,9 +1,10 @@
-#include <array>
 #include <algorithm>
+#include <boost/program_options.hpp>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <mpi/mpi.h>
+#include <stdexcept>
 #include <span>
 #include <ranges>
 #include <regex>
@@ -11,16 +12,53 @@
 
 #include "../include/mpi_histo.hpp"
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[])  {
 	MPI_Init(&argc, &argv);
+
+	po::options_description desc("Permitted options");
+	desc.add_options()
+		("help,h", "Show the help message")
+		("path,p", po::value<std::string>(), "Required path to directory to calculate histogram, it should be relative to the executable path")
+		("size,s", po::value<int>(), "Size of histogram");
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << desc << '\n';
+		MPI_Finalize();
+		return 1;
+	}
+
 	const std::filesystem::path current_dir = std::filesystem::current_path();
-	const std::filesystem::path label_path = current_dir/"labels";
+	std::filesystem::path label_path;
+	if (vm.count("path")) {
+		label_path = current_dir/(vm["path"].as<std::string>());
+		if (!std::filesystem::exists(label_path)) {
+			std::string err_msg = "Path "+label_path.native()+" does not exist";
+			MPI_Finalize();
+			throw std::invalid_argument(err_msg);
+		}
+	}
+	else {
+		MPI_Finalize();
+		throw std::invalid_argument("The (--path/-p) command line argument is required");
+	}
+
+	if (!vm.count("size")) {
+		throw std::invalid_argument("The (--size/-s) command line argument is required");
+	}
+
+	const int histo_size = vm["size"].as<int>();
+
 	const std::filesystem::directory_iterator label_dir{label_path};
-	const std::filesystem::directory_iterator label_dir_copy{label_path};
 	const std::vector<std::filesystem::directory_entry> files_in_dir(
-		begin(label_dir_copy), end(label_dir_copy)
+		begin(label_dir), end(label_dir)
 	);
-	const std::regex expr{"([0-9]{1,2}+)"};
+	const std::regex expr{"\\d+"};
 
 	int proc, num_procs;
 	MPI_Comm_rank(MPI_COMM_WORLD, &proc);
@@ -28,12 +66,9 @@ int main(int argc, char* argv[])  {
 	int iter_bound = files_in_dir.size() / num_procs;
 	const int start_idx = iter_bound * proc;
 
-	std::array<int, 30> histo;
-	std::array<int, 30> mpi_core_histo;
-	std::array<int, 30> mpi_root_histo;
-	std::ranges::for_each(histo, [](int& x) { x=0; });
-	std::ranges::for_each(mpi_core_histo, [](int& x) { x=0; });
-	std::ranges::for_each(mpi_root_histo, [](int& x) { x=0; });
+	std::vector<int> histo(histo_size, 0);
+	std::vector<int> mpi_core_histo(histo_size, 0);
+	std::vector<int> mpi_root_histo(histo_size, 0);
 
 	if ((iter_bound % num_procs != 0) && (proc == (num_procs-1))) {
 		iter_bound += (iter_bound % num_procs) + 1;
@@ -48,15 +83,6 @@ int main(int argc, char* argv[])  {
 	);
 
 	if (proc == 0) {
-		std::cout << "MPI histo:\n\t";
-		std::ranges::for_each(
-			mpi_root_histo,
-			[](const int& x) {
-				std::cout << x << ' ';
-			}
-		);
-		std::cout << '\n';
-
 		std::ofstream outfile;
 		outfile.open("histo_data.txt");
 		if (outfile.is_open()) {
