df7a3e83cf7ff2a9a1d87d3830ee0c4a26a0dc2e
Francisco Viramontes <fviramontes8@unm.edu>
2023-02-08 22:19:54 -0700

    Update label_visualizer
    Script now automatically gets the number of classes based off of the
    contents of the file read

--- label_visualizer.py
+++ label_visualizer.py
@@ -2,7 +2,7 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
-def parse_opt():
+def parse_opt() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Script to plot data from mpi_label_histo"
     )
@@ -19,11 +19,10 @@ def parse_opt():
 def main(opt: argparse.Namespace) -> None:
     with open(opt.file_path) as infile:
         file_data = infile.readlines()
-        list_data = file_data[0].split(' ')
+        list_data = file_data[0].split(' ')[:-1]
 
-    list_data = list_data[:30]
     np_data = np.array(list_data, dtype=int)
-    classes = [ i for i in range(30) ]
+    classes = [ i for i in range(len(np_data)) ]
 
     plt.bar(classes, np_data)
     plt.title("Histogram of classes")
